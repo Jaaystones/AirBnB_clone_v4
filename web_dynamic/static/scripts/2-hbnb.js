3c69d852af384ba6111dbb430f0141b2f26d2991
Jay stones <Joelpaul345@gmail.com>
2023-05-16 14:47:54 +0100

    Update 2-hbnb.js

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,34 +1,19 @@
-$(document).ready(function() {
-	  // Variable to store the checked amenities
-	var checkedAmenities = [];
-	function updateAmenities() {
-	  $('.popover .amenities h4').text(checkedAmenities.join(', '));
-	}
-	// Listen for changes on each input checkbox tag
-	$('.popover .amenities input[type="checkbox"]').change(function() {
-	  var amenityId = $(this).data('id');
-	  var amenityName = $(this).data('name');
-	
-	if ($(this).is(':checked')) {
-	  checkedAmenities.push(amenityId);
-	} else {
-	  var index = checkedAmenities.indexOf(amenityId);
-	  if (index > -1) {
-	    checkedAmenities.splice(index, 1);
-	  }
-	}
-	
-	                                                                                   // Update the h4 tag with the list of checked amenities
-	updateAmenities();
-	});
-	
+$(document).ready(function(){
+  $.get("http://0.0.0.0:5001/api/v1/status/", function(data){
+    if (data.status == "OK") {
+      $("DIV#api_status").addClass("available");
+    } else {
+      $("DIV#api_status").removeClass("available");
+    }
+  });
 
-	// Request API status
-	$.get('http://0.0.0.0:5001/api/v1/status/', function(data) {
-	  if (data.status === 'OK') {
-	    $('#api_status').addClass('available');
-	  } else {
-	    $('#api_status').removeClass('available');
-	  }
-	});
+  const amenities = {};
+    $("li input[type=checkbox]").change(function () {
+      if (this.checked) {
+        amenities[this.dataset.name] = this.dataset.id;
+      } else {
+        delete amenities[this.dataset.name];
+      }
+        $(".amenities h4").text(Object.keys(amenities).sort().join(", "));
+  });
 });
